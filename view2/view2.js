4e570cce079f75d460565e60e6fa713dcb096ebf
史安琪 <shianqi@imudges.com>
2015-10-28 23:16:54 +0800

    历史访问人数成功
    历史访问人数成功

--- view2/view2.js
+++ view2/view2.js
@@ -10,14 +10,16 @@ angular.module('myApp.view2', ['ngRoute'])
   }])
 
   .controller('View2Ctrl', ["$scope","$http",function($scope,$http) {
-      $.ajax({
-        type: "GET",
-        url: "http://115.28.72.26/visitNumber.php",
-        success:function(msg){
-           $scope.visitNumber=1;
-        },
-        error:function(){
-        }
+      //$.ajax({
+      //  type: "GET",
+      //  url: "http://115.28.72.26/visitNumber.php",
+      //  success:function(msg){
+      //     $scope.visitNumber=1;
+      //  },
+      //  error:function(){
+      //  }
+      //});
+      $http.get("http://115.28.72.26/visitNumber.php").success(function(response) {
+        $scope.visitNumber = response.number;
       });
-
   }]);
