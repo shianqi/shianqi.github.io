bdd87f141eee158293e33f77d404ae1a42f70da1
史安琪 <shianqi@imudges.com>
2015-10-28 20:26:29 +0800

    增加历史访问功能
    增加历史访问功能

--- app.js
+++ app.js
@@ -4,41 +4,46 @@
 angular.module('myApp', ['ngRoute', 'myApp.view1', 'myApp.view2', 'myApp.view3', 'myApp.version'])
     .config(['$routeProvider', function($routeProvider) {
       $routeProvider.otherwise({redirectTo: '/view1'});
-      }])
+    }])
 
 
-    .controller('title',  function($scope,$location){
-      $scope.isTrue=function(){
+    .controller('title',  function($scope,$location) {
+      $scope.isTrue = function () {
         return true;
       }
-      $scope.changeColor_1=function(){
-        window.location.href='index.html#/view1';
-        $scope.onOffState1='On';
-        $scope.onOffState2='Off';
-        $scope.onOffState3='Off';
+      $scope.changeColor_1 = function () {
+        window.location.href = 'index.html#/view1';
+        $scope.onOffState1 = 'On';
+        $scope.onOffState2 = 'Off';
+        $scope.onOffState3 = 'Off';
       }
-      $scope.changeColor_2=function(){
-        window.location.href='index.html#/view2';
-        $scope.onOffState1='Off';
-        $scope.onOffState2='On';
-        $scope.onOffState3='Off';
+      $scope.changeColor_2 = function () {
+        window.location.href = 'index.html#/view2';
+        $scope.onOffState1 = 'Off';
+        $scope.onOffState2 = 'On';
+        $scope.onOffState3 = 'Off';
       }
-      $scope.changeColor_3=function(){
-        window.location.href='index.html#/view3';
-        $scope.onOffState1='Off';
-        $scope.onOffState2='Off';
-        $scope.onOffState3='On';
+      $scope.changeColor_3 = function () {
+        window.location.href = 'index.html#/view3';
+        $scope.onOffState1 = 'Off';
+        $scope.onOffState2 = 'Off';
+        $scope.onOffState3 = 'On';
       }
 
-      if($location.url()=="/view3"){
-        $scope.onOffState1='Off';
-        $scope.onOffState2='Off';
-        $scope.onOffState3='On';
+      if ($location.url() == "/view3") {
+        $scope.onOffState1 = 'Off';
+        $scope.onOffState2 = 'Off';
+        $scope.onOffState3 = 'On';
       }
-      if($location.url()=="/view2"){
-        $scope.onOffState1='Off';
-        $scope.onOffState2='On';
-        $scope.onOffState3='Off';
+      if ($location.url() == "/view2") {
+        $scope.onOffState1 = 'Off';
+        $scope.onOffState2 = 'On';
+        $scope.onOffState3 = 'Off';
       }
+
+      $.ajax({
+        type: "POST",
+        url: "http://115.28.72.26/addVisit.php"
+      })
     });
 
